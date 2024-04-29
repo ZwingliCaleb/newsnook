217b1a914593d95e3a0db27d858f258dd866c62f
ZwingliCaleb <calebzwingli@gmail.com>
2024-04-29 08:09:27 +0300

    temporarily final styling of the searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -16,21 +16,23 @@ const SearchBar: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="relative gap-10">
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchQuery}
-          onChange={handleInputChange}
-          className="pl-10 pr-3 py-2 rounded-full focus:outline-none font-semibold focus:ring focus:border-blue-300 w-[600px] h-14"
-        />
-        <FontAwesomeIcon icon={faSearch} className="text-gray-600 absolute left-[560px] top-1/2 transform -translate-y-1/2" />
-      </div>
-      <button type="submit" className="absolute top-1/2 transform -translate-y-1/2 right-3 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-2xl focus:outline-none focus:ring focus:border-blue-300">
-        Search
-      </button>
-    </form>
+    <div className="flex justify-center">
+      <form onSubmit={handleSubmit} className="relative flex items-center">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={handleInputChange}
+            className="pl-6 pr-10 py-2 rounded-full focus:outline-none font-semibold focus:ring focus:border-blue-300 w-[600px] h-14"
+          />
+          <FontAwesomeIcon icon={faSearch} className="text-gray-600 absolute right-6 top-1/2 transform -translate-y-1/2" />
+        </div>
+        <button type="submit" className="ml-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-2xl focus:outline-none focus:ring focus:border-blue-300">
+          Search
+        </button>
+      </form>
+    </div>
   );
 };
 
