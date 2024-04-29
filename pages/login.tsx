d6eec0af39367e07d4701c037b11a7f9d8c31e9b
ZwingliCaleb <calebzwingli@gmail.com>
2024-04-29 08:49:17 +0300

    Building up the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
-const login = () => {
+const login: React.FC = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <section className="bg-white p-8 rounded-lg shadow-md flex flex-col md:flex:row md:max-w-4xl">
+        {/*left card*/}
+        <div className="md:w-1/2 bg-blue-500 text-white flex items-center justify-center">
+          <div className="text-center">
+            <h2 className="text-3xl font-semibold mb-4">Welcome</h2>
+            <p className="text-lg">Lorem Ipsum dolor sit amet</p>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
