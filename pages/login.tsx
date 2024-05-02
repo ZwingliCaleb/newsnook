22e8afec92a74b78b62cbbf44e2b59b963dbe7d1
ZwingliCaleb <calebzwingli@gmail.com>
2024-05-02 14:30:38 +0300

    tentatively finalize on Index, Login and Signup UI styling

--- pages/login.tsx
+++ pages/login.tsx
@@ -28,7 +28,7 @@ const Login: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
       <div className="w-3/4 h-3/4 border border-gray-200 shadow-2xl flex flex-wrap relative">
-        <div className="w-full md:w-1/2 bg-cover bg-center relative" style={{ backgroundImage: 'url(/images/LoginImage.jpg)' }}>
+        <div className="w-full p-20 md:w-1/2 bg-cover bg-center relative" style={{ backgroundImage: 'url(/images/LoginImage.jpg)' }}>
           <div className="absolute inset-0 bg-black opacity-60"></div>
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
             <h2 className="text-3xl font-bold">Welcome!</h2>
@@ -36,58 +36,60 @@ const Login: React.FC = () => {
             <p className="text-md font-semibold">One stop spot to manage all your vending functions</p>
           </div>
         </div>
-        <div className="w-full md:w-1/2 bg-green-300 p-6 flex items-center justify-center">
+        <div className="w-full md:w-1/2 bg-gradient-to-t from-slate-200 to-sky-200 p-6 flex items-center justify-center">
           <form onSubmit={handleSubmit} className="w-full max-w-md">
             <div className="mb-4">
-              <label htmlFor="email" className="block text-gray-600">Email</label>
+              <label htmlFor="email" className="block font-semibold text-gray-700">Email</label>
               <input
                 type="email"
                 id="email"
                 value={email}
                 onChange={handleEmailChange}
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-green-500"
+                className="w-full px-4 py-2 border shadow-md rounded-md focus:outline-none focus:border-sky-500"
                 required
               />
             </div>
             <div className="mb-4 relative">
-              <label htmlFor="password" className="block text-gray-600">Password</label>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                id="password"
-                value={password}
-                onChange={handlePasswordChange}
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-green-500"
-                required
-              />
-
-              <div className="mt-2">
-                <a href="#" className="text-blue-500 hover:underline">Forgot password?</a>
+              <label htmlFor="password" className="block font-semibold text-gray-700">Password</label>
+              <div className="relative">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="w-full px-4 py-2 border shadow-md rounded-md focus:outline-none focus:border-sky-500"
+                  required
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center focus:outline-none"
+                  onClick={handleTogglePasswordVisibility}
+                >
+                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-gray-600" />
+                </button>
               </div>
-
-              <button
-                type="button"
-                className="absolute inset-y-0 right-0 pr-3 flex items-center focus:outline-none"
-                onClick={handleTogglePasswordVisibility}
-              >
-                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-gray-600 px-2" />
-              </button>
+              <div className="mt-2">
+                <a href="#" className="text-blue-600 hover:underline">Forgot password?</a>
+              </div>
             </div>
 
-            <div className="flex gap-4">
+            <div className="">
               <button
                 type="submit"
-                className="px-8 py-2 h-12 bg-green-500 hover:bg-green-600 text-white  font-semibold shadow-md rounded-md focus:outline-none focus:ring focus:border-green-300"
+                className="py-2 bg-blue-500 hover:bg-blue-600 text-white  font-semibold shadow-md rounded-md focus:outline-none focus:ring focus:border-green-300 w-full"
               >
                Login
               </button>
-              <button className=" bg-white text-gray-800 font-semibold py-2 px-8 h-12 rounded-md shadow-md hover:bg-gray-100">
+              <p className="mt-4 text-center font-bold">or</p>
+              <button className="bg-white mt-4 text-gray-800 font-semibold w-full py-2 rounded-md shadow-md hover:bg-gray-100">
+                <img src="/icons/google.svg" alt="Google Logo" className="w-5 h-5 inline-block mr-2" />
                 Sign in with Google
               </button>
             </div>
 
             <div className="mt-4 flex p-2 gap-2">
               <p>Have no account yet? </p>
-              <a href="/signup" className="text-blue-500 hover:underline"> Sign up</a>
+              <a href="/signup" className="font-semibold text-blue-700 hover:underline"> Sign up</a>
             </div>
             
           </form>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Footer from '@/components/Footer'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-
-const signup: React.FC = () => {
+const Signup: React.FC = () => {
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+
+  const handleFullNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFullName(e.target.value);
+  };
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -23,79 +27,81 @@ const signup: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Login credentials:', { email, password });
+    console.log('Signup credentials:', { fullName, email, password });
   };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-    <div className="w-3/4 h-3/4 border border-gray-200 shadow-2xl flex flex-wrap relative">
-      <div className="w-full md:w-1/2 bg-cover bg-center relative" style={{ backgroundImage: 'url(/images/LoginImage.jpg)' }}>
-        <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h2 className="text-3xl font-semibold">Welcome</h2>
-          <p className="text-lg">One stop spot to Manage all your Vending Work</p>
-          <div className="mt-4">
-            <a href="#" className="text-blue-500 hover:underline">Sign up</a> | <a href="#" className="text-blue-500 hover:underline">Forgot password?</a>
+      <div className="w-3/4 h-3/4 border border-gray-200 shadow-2xl flex flex-wrap relative">
+        <div className="w-full md:w-1/2 relative p-20 bg-cover bg-center" style={{ backgroundImage: 'url(/images/LoginImage.jpg)' }}>
+          <div className="absolute inset-0 bg-black opacity-60"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+            <h2 className="text-3xl font-semibold">Welcome</h2>
+            <p className="text-lg">One stop spot to Manage all your Vending Work</p>
           </div>
-          <button className="mt-4 bg-white text-gray-800 py-2 px-4 rounded-md shadow-md hover:bg-gray-100">
-            Sign in with Google
-          </button>
+        </div>
+        <div className="w-full md:w-1/2 bg-gradient-to-b from-sky-100 to-slate-300 p-8 flex items-center justify-center">
+          <form onSubmit={handleSubmit} className="w-full max-w-md">
+            <div className="mb-4">
+              <label htmlFor="fullName" className="block text-gray-700 font-semibold">Full Name</label>
+              <input
+                type="text"
+                id="fullName"
+                value={fullName}
+                onChange={handleFullNameChange}
+                className="w-full px-4 py-2 border shadow-md rounded-md focus:outline-none focus:border-sky-500"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700 font-semibold">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                className="w-full px-4 py-2 border shadow-md rounded-md focus:outline-none focus:border-sky-500"
+                required
+              />
+            </div>
+            <div className="mb-4 relative">
+              <label htmlFor="password" className="block text-gray-700 font-semibold">Password</label>
+              <div className="relative">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="w-full px-4 py-2 border shadow-md rounded-md focus:outline-none focus:border-sky-500"
+                  required
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center focus:outline-none"
+                  onClick={handleTogglePasswordVisibility}
+                >
+                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-gray-600" />
+                </button>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full shadow-md bg-sky-500 hover:bg-sky-600 text-white font-semibold py-2 rounded-md focus:outline-none focus:ring focus:border-green-300"
+            >
+              Sign Up
+            </button>
+            <p className="text-center p-2 font-semibold"> or </p>
+            <button className="mt- bg-white text-gray-800 font-semibold py-2 rounded-md shadow-md hover:bg-gray-100 w-full">
+              <img src="/icons/google.svg" alt="Google Logo" className="w-5 h-5 inline-block mr-2" />
+              Sign up with Google
+            </button>
+            <p className="mt-4 text-gray-600">Already have an account? <a href="login" className="font-semibold text-blue-700 hover:underline">Login</a></p>
+          </form>
         </div>
       </div>
-      <div className="w-full md:w-1/2 bg-green-300 p-8 flex items-center justify-center">
-        <form onSubmit={handleSubmit} className="w-full max-w-md">
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-600">Email</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-green-500"
-              required
-            />
-          </div>
-          <div className="mb-4 relative">
-            <label htmlFor="password" className="block text-gray-600">Password</label>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-green-500"
-              required
-            />
-
-            <label htmlFor="password" className="block text-gray-600 mt-4">Retype Password</label>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="w-full mt-2px-4 py-2 border rounded-md focus:outline-none focus:border-green-500"
-              required
-            />
-
-            <button
-              type="button"
-              className="absolute inset-y-0 right-0 pr-3 flex items-center focus:outline-none"
-              onClick={handleTogglePasswordVisibility}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-gray-600" />
-            </button>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-green-500 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-green-300"
-          >
-            Login
-          </button>
-        </form>
-      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
   )
 }
 
-export default signup
+export default Signup;
